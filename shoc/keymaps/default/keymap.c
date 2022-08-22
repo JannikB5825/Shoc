9cadafe21ddb5eb993c2f0485d64a34445af4a8b
JannikB5825 <jannik.becker@bender.de>
2022-08-22 20:49:05 +0200

    Letzte Firmware

--- shoc/keymaps/default/keymap.c
+++ shoc/keymaps/default/keymap.c
@@ -5,52 +5,28 @@ char wpm_str[32];
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* 0
- * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬─────────────┐
- * │ Esc  │   1  │   2  │   3  │   4  │   5  │   6  │   7  │   8  │   9  │   0  │   -  │     BSPC    │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┬──────┤
- * │ Tab  │   Q  │   W  │   E  │   R  │   T  │   Y  │   U  │   I  │   O  │   P  │  {   │  }   │  \   │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┴──────┤
- * │ Caps │   A  │   S  │   D  │   F  │   G  │   H  │   J  │   K  │   L  │   ;  │   '  │    Enter    │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼─────────────┤
- * │ Shft │   Z  │   X  │   C  │   V  │   B  │   N  │   M  │   ,  │   .  │   /  │  Up  │      =      │
- * ├──────┼──────┼──────┼──────┴──────┼──────┼──────┴──────┼──────┼──────┼──────┼──────┼─────────────┤
- * │ Ctrl │ Gui  │ Alt  │    Space    │   1  │    Space    │ RALT │ Ctrl │ Left │ Down │    Right    │
- * └──────┴──────┴──────┴─────────────┴──────┴─────────────┴──────┴──────┴──────┴──────┴─────────────┘
- */
 	[0] = LAYOUT(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_BSPC,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LCBR, KC_RCBR, KC_BSLS,
-        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_EQL,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, MO(1), KC_SPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
-	),
+		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U,
+		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J,
+		KC_LSFT, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1), KC_SPC, KC_RALT, KC_RCTL,
+		KC_BSPC, KC_MINS, KC_0, KC_9, KC_8, KC_I, KC_O, KC_P,
+		KC_RGHT, KC_DOWN, KC_LEFT, KC_EQL, KC_UP, KC_QUES, KC_COMM, KC_DOT,
+		KC_RBRC, KC_LBRC, KC_BSLS, KC_ENT, KC_DQUO, KC_SCLN, KC_L, KC_K),
 
-/* 1
- * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬─────────────┐
- * │ Esc  │  F1  │  F2  │  F3  │  F4  │  F5  │  F6  │  F7  │  F8  │  F9  │  F10 │  F11 │     F12     │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┬──────┤
- * │ Tab  │   Q  │   W  │   E  │   R  │   T  │   Y  │   U  │   I  │   O  │   P  │  {   │  }   │  \   │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┴──────┤
- * │ Caps │   A  │   S  │   D  │   F  │   G  │   H  │   J  │   K  │   L  │   ;  │   '  │    Enter    │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼─────────────┤
- * │ Shft │   Z  │   X  │   C  │   V  │   B  │   N  │   M  │   ,  │   .  │   /  │  Up  │     Del     │
- * ├──────┼──────┼──────┼──────┴──────┼──────┼──────┴──────┼──────┼──────┼──────┼──────┼─────────────┤
- * │ Ctrl │ Gui  │ Alt  │    Space    │   1  │    Space    │ RALT │ Ctrl │ Left │ Down │    Right    │
- * └──────┴──────┴──────┴─────────────┴──────┴─────────────┴──────┴──────┴──────┴──────┴─────────────┘
- */
-	[1] = LAYOUT(
-        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LCBR, KC_RCBR, KC_BSLS,
-        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_DEL,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, MO(1), KC_SPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
-	)
+    [1] = LAYOUT(
+		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U,
+		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J,
+		KC_LSFT, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1), KC_SPC, KC_RALT, KC_RCTL,
+		KC_BSPC, KC_F11, KC_F10, KC_F9, KC_F8, KC_I, KC_O, KC_P,
+		KC_RGHT, KC_DOWN, KC_LEFT, KC_DEL, KC_UP, KC_QUES, KC_COMM, KC_DOT,
+		KC_RBRC, KC_LBRC, KC_BSLS, KC_ENT, KC_DQUO, KC_SCLN, KC_L, KC_K)
 
 
 };
-
-
 #ifdef OLED_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
